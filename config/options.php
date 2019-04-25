b9553c7b4bc2892cdde4e115de0e16f819d9cc21
James Watkins <james@bitslash.com>
2019-04-24 21:51:25 -0400

    added stars to the fundraiser reviews

---
+++ config/options.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * options available across the site
+ */
+return [
+    // options for ratings
+    'ratings' => [
+        // the number of stars available for reviews.rating
+        'star_count' => 5
+    ]
+];
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,7 +11,14 @@
                         <h1>{{ $review->fundraiser_name }}</h1>
                     </div>
                     <div class="panel-body">
-                        {{ $review->reviewer_name }}
+                        @for ($i = 1; $i <= Config::get('options.ratings.star_count'); $i++)
+                            @if ($i <= $review->rating)
+                                <i class="fa fa-star"></i>
+                            @else
+                                <i class="fa fa-star-o"></i>
+                            @endif
+                        @endfor
+                        <p>{{ $review->reviewer_name }}</p>
                     </div>
                 </div>
             </div>
