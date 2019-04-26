2b06516e9b5b347a425e627a98a7029d98a4d05a
James Watkins <james@bitslash.com>
2019-04-26 02:53:28 -0400

    fixed margins on the home page for better responsiveness

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,31 +6,29 @@
     @foreach ($reviews as $review)
         <div class="row">
             <div class="col-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <i class="fa fa-certificate fa-5x pull-left"></i>
-                        <h1>{{ $review->fundraiser_name }}</h1>
-                    </div>
-                    <div class="panel-body">
-                        @for ($i = 1; $i <= Config::get('options.ratings.star_count'); $i++)
-                            @if ($review->average_rating >= $i)
-                                <i class="fa fa-star"></i>
-                            @elseif ($review->average_rating >= ($i - Config::get('options.ratings.half_star')))
-                                <i class="fa fa-star-half-o"></i>
-                            @else
-                                <i class="fa fa-star-o"></i>
-                            @endif
-                        @endfor
-                        <a href="{{ url('/list/' . $review->fundraiser_id) }}">
-                            @if ($review->num_rating == 1)
-                                ({{ $review->num_rating }} Review)
-                            @else
-                                ({{ $review->num_rating }} Reviews)
-                            @endif
-                        </a>
-                        <br />
-                        <a class="rate" href="{{ url('rate/' . $review->fundraiser_id) }}">Rate this Fundraiser</a>
-                    </div>
+                <div class="pull-left certificate">
+                    <i class="fa fa-certificate fa-5x"></i>
+                </div>
+                <div class="fundraiser">
+                    <h1>{{ $review->fundraiser_name }}</h1>
+                    @for ($i = 1; $i <= Config::get('options.ratings.star_count'); $i++)
+                        @if ($review->average_rating >= $i)
+                            <i class="fa fa-star"></i>
+                        @elseif ($review->average_rating >= ($i - Config::get('options.ratings.half_star')))
+                            <i class="fa fa-star-half-o"></i>
+                        @else
+                            <i class="fa fa-star-o"></i>
+                        @endif
+                    @endfor
+                    <a href="{{ url('/list/' . $review->fundraiser_id) }}">
+                        @if ($review->num_rating == 1)
+                            ({{ $review->num_rating }} Review)
+                        @else
+                            ({{ $review->num_rating }} Reviews)
+                        @endif
+                    </a>
+                    <br />
+                    <a href="{{ url('rate/' . $review->fundraiser_id) }}">Rate this Fundraiser</a>
                 </div>
             </div>
         </div>
